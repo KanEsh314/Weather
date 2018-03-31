66859dceb9eb678fd2ffb313f9da50d015d6abcd
KanEsh314 <kanesh314@gmail.com>
2018-03-31 12:31:20 +0800

    Ionic Class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'; 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -14,6 +17,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  weather: any = []
+  city : any = []
+  list : any = []
 
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.weather = this.http.get("http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=9fd7a449d055dba26a982a3220f32aa2").subscribe(data => {
+      //console.log(data["city"]);
+      // console.log(data["list"])
+      this.city = data["city"];
+      this.list = data["list"];
+    }, (err) => {
+      console.log(err);
+    })
   }
-
 }
